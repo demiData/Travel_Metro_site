42ab87eeb01765ea560d6c6809690e49735498c9
Demi Dennise Minjarez <demicode@Demis-MacBook-Pro.local>
2016-05-26 13:45:54 -0700

    adding all change

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+ 
+     // Index.html Img Slider  
+      $('#slider').cycle({
+       fx:     'scrollHorz',
+       next:    '#next',
+       prev:    '#prev',
+       timeout: 3000,
+       pause:   1
+    });
+      
+   // Hover effect
+
+    $('.article-item').hover( function() {
+        $(this).find('.img-title').fadeIn(300);
+    }, function() {
+        $(this).find('.img-title').fadeOut(100);
+    });
+
+
+
+
+});
